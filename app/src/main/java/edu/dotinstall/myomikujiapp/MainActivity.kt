d19dd3f08b86155849330d7333942386d998e847
濱野将士 <masashihamano@mhp508.ad.nerc.ac.uk>
2018-02-03 01:40:16 +0530

    android application9

---
+++ app/src/main/java/edu/dotinstall/myomikujiapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package edu.dotinstall.myomikujiapp
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getOmikujiButton.setOnClickListener {
+//            Log.v("MAinActivity", "Button Clicked")
+            val results = arrayOf("大吉","吉","中吉","小吉","凶")
+
+//            val n = Random().nextInt(5)
+            val n = Random().nextInt(results.count())
+
+            if (n == 0) {
+//                resultTextView.setTextColor(Color.RED)
+//                resultTextView.setTextColor(Color.parseColor( "#ff0000"))
+                resultTextView.setTextColor(Color.argb( 255,255,0,0))
+            } else {
+                resultTextView.setTextColor(Color.parseColor("#808080"))
+            }
+
+            resultTextView.text = results.get(n)
+        }
+    }
+}
